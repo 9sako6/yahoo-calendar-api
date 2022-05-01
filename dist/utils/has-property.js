a70eac633905f7dd502ebe8962c0ff9f0e9f0df8
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-05-01 14:41:52 +0900

    Build with Vite (#11)

--- dist/utils/has-property.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hasProperty = void 0;
-function hasProperty(obj, key) {
-  return obj instanceof Object && key in obj;
-}
-exports.hasProperty = hasProperty;
