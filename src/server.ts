02d3f13c0fa2ddb8b411ebf301027cb6e4279ee1
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-05-02 16:23:00 +0900

    Build with Vite and vite-plugin-node
    ## References
    
    - https://scrapbox.io/dojineko/Vite%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6Express.js%E3%82%A2%E3%83%97%E3%83%AA%E3%82%92%E4%BD%9C%E3%82%8B
    - https://github.com/axe-me/vite-plugin-node

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { router } from "./router";
 import { API_VERSION } from "./version";
 import http from "http";
 
-export const API_ENDPOINT = `/api/${API_VERSION}`;
+const API_ENDPOINT = `/api/${API_VERSION}`;
 
 const app = createExpressApplication();
 
@@ -14,14 +14,15 @@ app.use(API_ENDPOINT, router);
 const server = http.createServer(app);
 
 // Connection with WebSocket
-server.on("upgrade", handleUpgrade);
+server.on("upgrade", (req, socket, head) => handleUpgrade(req, socket, head));
 
 const port = process.env.PORT || 8080;
 
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV === "production") {
   server.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
   });
 }
 
 export { app };
+export const viteNodeApp = app;
